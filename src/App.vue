3174dc6c2a37bc1374d9b7858048b1db6b25cae9
Dylan <dylan.matab99@gmail.com>
2019-11-04 19:03:55 +0100

    Added create account form

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,7 @@ export default {
     //
   }),
   mounted() {
-    this.$router.push({name: 'login'})
+    this.$router.push({name: 'createAccount'})
   }
 };
 </script>
--- src/views/CreateAccount.vue
+++ src/views/CreateAccount.vue
@@ -1,13 +1,29 @@
 <template>
-    <v-container>
-
-    </v-container>
+  <v-container fluid class="justify-center fill-height">
+    <v-card class="create-account-card py-6" width="575px">
+      <v-row justify="center" align="center">
+        <v-col cols="7" class="text-center">
+          <span class="headline">
+            Create account
+          </span>
+          <v-text-field label="Full name"></v-text-field>
+          <v-text-field label="Email"></v-text-field>
+          <v-text-field label="Password"></v-text-field>
+          <v-text-field label="Repeat password"></v-text-field>
+          <v-btn block color="primary">Create</v-btn>
+            <div class="text-left pt-2">
+                <a href="#">Already have an account?</a>
+            </div>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: "CreateAccount"
+  name: 'CreateAccount',
 };
 </script>
 
-<style scoped></style>
+<style lang="scss"></style>
